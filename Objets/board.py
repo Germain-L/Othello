64405efd17b0452dbfe0f0ce6d55778927a1797b
Germain Leignel <germain.leignel@gmail.com>
2021-03-08 10:36:37 +0100

    update

--- Objets/board.py
+++ Objets/board.py
@@ -7,15 +7,26 @@ class Board:
 
     def generate(self):
         #TODO DELETE
-        self.size += 2
         rows = ["•" for i in range(self.size)]
         self.board = [rows for i in range(self.size)]
 
-    #TODO DELETE
     def display(self):
-        for x in range(self.size):
-            for y in range(self.size):
-                print(self.board[x][y])
+        """Displays the board in a grid"""
+        #TODO DELETE
+
+        nums = [i for i in range(1, self.size +1)]
+        
+        print(" ", end="")
+        for i in nums:
+            print(f" {i}", end="")
+        print()
+
+        for x in range(len(self.board)):
+            print(nums[x], end=" ")
+
+            for y in range(len(self.board[x])):
+                print(self.board[x][y], end=" ")
+            print()
 
     def place(self, x, y, pawn):
         #TODO place pawn in a case
